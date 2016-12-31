3cf40df7cad93181f29b26abd5df748d59df5205
PWill21 <patrick.j.will@gmail.com>
2016-12-31 01:30:04 -0500

    added code to disply result of roll(s)

--- lrc.py
+++ lrc.py
@@ -11,7 +11,15 @@ import random
 def rollDice(number): # number is the number of dice the user is allowed to roll
     for x in range(1,number + 1):
         roll = random.randint(1,6)
-        print(roll)
-        #return roll
+        if (roll >=1) and (roll <= 3):
+            result = "Hold"
+        elif roll == 4:
+            result = "Left"
+        elif roll == 5:
+            result = "Right"
+        else:
+            result = "Center"
+        print(result)
+        #return result
 
-rollDice(1)
+rollDice(3)
